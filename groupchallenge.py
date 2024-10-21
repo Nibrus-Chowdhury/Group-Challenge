1af837f35127c77f4789047c8e33e1190f9abafe
Nibrus Chowdhury <147645175+Nibrus-Chowdhury@users.noreply.github.com>
2024-10-21 12:48:41 -0400

    Update groupchallenge.py

--- groupchallenge.py
+++ groupchallenge.py
@@ -5,6 +5,7 @@ Period 5-6
 Description: User guesses a random number from 1 to 10 and the computer responds if that is high or low until the user gets the answer 
 """
 import random
+import time
 
 rounds = int(input("How many rounds do you want to play?"))
 difficulty = input("How difficult do you want it? (1-10, 1-50, 1-1000)")
@@ -23,11 +24,15 @@ def attempt(ran_num, attempts, rounds):
         random_num = ran_num3
     if rounds > 0:
         user_guess = int(input("What is your guess? 1-10"))
+        time1 = time.time()
         if user_guess == ran_num:
             print(f"It took you: {attempts} attempts to guess the number")
             rounds -= 1
+            time2 = time.time()
+            print('Response time: ', time2 - time1)
         else:
             attempts += 1
+            print('Response time: ', time2 - time1)
             if user_guess > ran_num:
                 print("Too high")
                 attempt(ran_num, attempts)
